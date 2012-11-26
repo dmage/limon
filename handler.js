c676133260ff4f9980dfec9a7e9e8f6fefc11381
Oleg Bulatov <oleg@bulatov.me>
2012-11-26 19:47:08 +0400

    Implement basic /api/write method
    Signed-off-by: Oleg Bulatov <oleg@bulatov.me>

--- handler.js
+++ handler.js
@@ -1,5 +1,22 @@
 exports = module.exports = function() {
-    return function handler(req, res) {
-        res.end('hello world\n');
+    var sqlite3 = require('sqlite3').verbose();
+    var db = new sqlite3.Database('./write.db');
+
+    db.run("CREATE TABLE data (id INTEGER PRIMARY KEY AUTOINCREMENT, key VARCHAR(255), timestamp INTEGER, value DOUBLE)", function(error) {
+        // ...
+    });
+
+    return function handler(req, res, next) {
+        if (/^\/api\/write(\?|$)/.test(req.url)) {
+            var key = req.query.key,
+                value = req.query.value;
+            db.serialize(function() {
+                var stmt = db.prepare("INSERT INTO data (key, timestamp, value) VALUES (?, ?, ?)");
+                stmt.run(key, Math.floor(new Date()/1000), value);
+            });
+            res.end('key: [' + key + '], value: [' + value + ']\n');
+        } else {
+            next();
+        }
     }
 }
